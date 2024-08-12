90f7346eb1e20922c5f0dd52368f32686c7d013d
fhgfhfdgh <suswnone@gmail.com>
2024-08-12 19:20:06 +0300

    Add files via upload

--- src.js
+++ src.js
@@ -1,3 +1,6 @@
+let tg = window.Telegram.WebApp;
+tg.expand() // метод позволяет растянуть окно на всю высоту.
+
 const circle = document.getElementById('clickable-circle');
 const scoreValue = document.getElementById('score-value');
 let score = localStorage.getItem('score') || 0;
